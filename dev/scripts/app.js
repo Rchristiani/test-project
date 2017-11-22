3d63d0cd149f3a504287db100cfba70d7e75f322
Ryan Christiani <ryan@ryanchristiani.com>
2017-11-21 19:16:23 -0500

    Adding donuts

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const donuts = ['Sourcream Glazed','Honey Dip','Boston Cream'];
+import Donut from './donut';
 
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          <header>
+            <h1>Donuts</h1>
+          </header>
+          <DonutList />
+          {donuts.map(donut => {
+           return (
+            <Donut donutName={donut} />
+           ) 
+          })}
+          <footer>
+            <p>donuts &copy; 2017</p>
+          </footer>
         </div>
       )
     }
